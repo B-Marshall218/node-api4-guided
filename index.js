cd036edd91477ba3ae5ead791d3887f29253537d
Brian <lawmen218@yahoo.com>
2020-07-15 22:43:00 -0400

    update message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server.js");
 
-console.log("node environment", process.env.NODE_ENV)
+// console.log("node environment", process.env.NODE_ENV)
 const port = process.env.PORT || 7000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
